881caa5279a1597778ad964e6f039387513386e6
yaroslav207 <75075327+yaroslav207@users.noreply.github.com>
2021-10-21 20:24:05 +0300

    create firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBm5XCpk_-K2kJd-MOQDkwdHdd6RMPyUsw',
+  authDomain: 'collection-list-74c13.firebaseapp.com',
+  projectId: 'collection-list-74c13',
+  storageBucket: 'collection-list-74c13.appspot.com',
+  messagingSenderId: '965091236563',
+  appId: '1:965091236563:web:fba303d4c5f72f11d05226',
+  measurementId: 'G-2LE2YYDJMW',
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
