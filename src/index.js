c6cb22f23fc8e23f6991b6800e1591efe163855f
yaroslav207 <75075327+yaroslav207@users.noreply.github.com>
2021-10-23 12:52:31 +0300

    add global styles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './components/app';
 import {Provider} from 'react-redux';
 import store from './store/store';
 import './styles/common.scss';
+import './assets/css/styles.scss';
 
 ReactDOM.render(
     <Provider store={store}>
